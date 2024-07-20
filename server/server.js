b024422847cc7b2dcd6caa508e4a7e5173d220af
Sachin <rahulit1708@gmail.com>
2024-07-21 00:22:16 +0530

    database connection added

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose')
+const database = require("./config/connection")
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8001;
 
+database.connect();
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
