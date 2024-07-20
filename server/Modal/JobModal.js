6fcda7b4ddd5d5d1aaedf48f08b320eaf1ca75c1
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-07-21 01:16:36 +0530

    Modals Merge conflict resolved

---
+++ server/Modal/JobModal.js
@@ -0,0 +1,36 @@
+import { mongoose } from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+  workerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Worker"
+  },
+  categoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category"
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  noOfHours: {
+    type: Number,
+    required: true
+  },
+  pincode: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  status: {
+    type: string,
+    enum: ['pending', 'completed', 'accepted', 'rejected'],
+    default: 'pending'
+  }
+})
+export const User = mongoose.model('Job', jobSchema);
+export default Job;
+
