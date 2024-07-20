59c6952e7383402b15ca5d53bacb87f454b16780
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-07-21 00:40:22 +0530

    CommonJs added

---
+++ server/Modal/ReviewModal.js
@@ -0,0 +1,8 @@
+const { mongoose } = require("mongoose");
+
+
+const ReviewModal = new mongoose.createSchema({
+
+});
+
+
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,17 +1,17 @@
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
-const  {MONGODB_URL} = process.env;
+const { MONGODB_URL } = process.env;
 
-exports.connect = ()=>{
-    mongoose.connect(MONGODB_URL,{
-        useNewUrlparser: true,
-        useUnifiedTopology: true,
-    })
-    .then(console.log(`DB Connection Success`))
+exports.connect = () => {
+  mongoose.connect(MONGODB_URL, {
+    useNewUrlparser: true,
+    useUnifiedTopology: true,
+  })
+    .then((c) => console.log(`DB Connection Success`))
     .catch((err) => {
-        console.log(`DB Connection Failed`);
-        console.log(err);
-        process.exit(1);
+      console.log(`DB Connection Failed`);
+      console.log(err);
+      process.exit(1);
     });
 }
 
